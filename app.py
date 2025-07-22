0487a1bbc9d4bc6728e566fda6f3436823fd8d5c
koushic <rshic14@gmail.com>
2025-07-22 08:06:10 +0530

    Initial commit

---
+++ app.py
@@ -0,0 +1,13 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET', 'POST'])
+def home():
+    username = None
+    if request.method == 'POST':
+        username = request.form.get('username')
+    return render_template('index.html', username=username)
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=10000)
